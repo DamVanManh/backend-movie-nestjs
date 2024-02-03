7428cd3c140c160f619b3929b88031fb97cbe009
hnammad7 <hnammad7gmail.com>
2024-02-03 15:19:19 +0700

    change structure folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { NguoiDungModule } from './nguoidung/nguoidung.module';
+import { NguoiDungModule } from './modules/nguoidung/nguoidung.module';
 import { ConfigModule } from '@nestjs/config';
 import { JwtStrategy } from './stratery/jwt.strategy';
-import { HinhanhModule } from './hinhanh/hinhanh.module';
-import { BinhluanModule } from './binhluan/binhluan.module';
-import { PhimModule } from './phim/phim.module';
+import { HinhanhModule } from './modules/hinhanh/hinhanh.module';
+import { BinhluanModule } from './modules/binhluan/binhluan.module';
+import { PhimModule } from './modules/phim/phim.module';
 // , HinhanhModule, BinhluanModule, LuuanhModule , NguoidungModule, AuthModule,
 @Module({
   imports: [
